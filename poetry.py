614f23019c6a88e557ff4836a7883c6c08534b79
Abhay Chaturvedi <chaturvediabhay24@gmail.com.com>
2019-11-21 00:59:41 +0530

    removed keras

--- poetry.py
+++ poetry.py
@@ -1,8 +1,6 @@
-from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D, MaxPooling2D, MaxPooling2D, Dense, Flatten
 
 import cv2 as cv
 
@@ -20,8 +18,8 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
 classifier.add(Flatten())
 
-classifier.add(Dense(output_dim = 128, activation = 'relu'))
-classifier.add(Dense(output_dim = 1, activation = 'sigmoid'))
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
 
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
@@ -30,7 +28,7 @@ classifier.load_weights('./Shelf_classifier.h5')
 
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.preprocessing import image
+from tensorflow.keras.preprocessing import image
 def predictor(img):
   # img = image.load_img(link,target_size=(64,64, 3))
   width=64
